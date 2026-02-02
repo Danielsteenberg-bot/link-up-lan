3874424b6ed1ce54dea5260fae604c00db61ed10
Daniel Steenberg <85222423+Danielsteenberg-bot@users.noreply.github.com>
2026-02-02 20:24:25 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/link-up-lan', 
+  base: '/link-up-lan/', 
   plugins: [react()],
 });
